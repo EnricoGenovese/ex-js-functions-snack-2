0e1e2c46919a0588cc693e1d75119196773f9c9d
EnricoGeno <enricogeno@libero.it>
2025-03-13 14:25:19 +0100

    snack_10 correction

--- snack_10/main.js
+++ snack_10/main.js
@@ -5,15 +5,21 @@
 // Restituisce una nuova funzione che, quando chiamata ripetutamente, esegue l'operazione originale al massimo una volta ogni n millisecondi.
 
 function creaThrottler(funzione, tempoLimite) {
+    let lastExecution = 0; // -> Punto di partenza per il calcolo del tempo trascorso dall'ultima esecuzione
+
     return function () {
-        setInterval(() => {
+        const now = Date.now()
+        if (now - lastExecution >= tempoLimite) { // -> Calcolo del tempo passato dall'ultima esecuzione del codice
+            lastExecution = now; // -> Nuovo valore del punto di partenza per il calcolo
             funzione()
-        }, tempoLimite)
+        } else {
+            console.log("Non posso ancora eseguire")
+        }
     };
 };
 
-const throttledLog = creaThrottler(() => console.log("Eseguito!"), 2000);
+const throttledLog = creaThrottler(() => console.log("Eseguito"), 2000);
 
-throttledLog(); // ✅ "Eseguito!"
-throttledLog(); // ❌ Ignorato (chiamato troppo presto)
-setTimeout(throttledLog, 2500); // ✅ "Eseguito!" (dopo 2.5 secondi)
+throttledLog(); // -> lastExecution è minore del limite, quindi si esegue
+throttledLog(); // -> Non parte perché non è ancora trascorso il tempo limite
+setTimeout(throttledLog, 2500); // --> Viene eseguito perché è passato abbastanza tempo 2.5s vs. 2s
